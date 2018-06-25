b0fc55e7a061b596b97173213da3040deae57e1f
conhy <conhysham@cox.net>
2018-06-24 21:19:34 -0700

    Functionality complete. Still needs styling.

---
+++ assets/javascript/app.js
@@ -0,0 +1,63 @@
+$(document).ready(function () {
+    var topics = ["Dog", "Cat", "Bird", "Whale", "Fish", "Dolphin", "Cow", "Bear", "Lion", "Monkey"];
+
+    function makeButtons() {
+        $("#buttonContainer").empty();
+        for (var i = 0; i < topics.length; i++) {
+            var button = $("<button>");
+            button.attr("class", "animalButton");
+            button.attr("animalType", topics[i]);
+            button.text(topics[i]);
+            $("#buttonContainer").append(button);
+        }
+    }
+
+    $("#submit").on("click", function (event) {
+        event.preventDefault();
+        var animal = $("#searchTerm").val().trim();
+        topics.push(animal);
+        //$("#searchTerm").text(" ");
+        makeButtons();
+    });
+
+
+    function makeGifs() {
+        $(".animalButton").on("click", function () {
+            var animal = $(this).attr("animalType");
+            var url = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=1TXRbCxeJtYKtlEcExz1Ift93INFAfMD";
+            $.ajax({
+                url: url,
+                method: "GET"
+            }).then(function (response) {
+                console.log(response);
+                var results = response.data;
+                for (var i = 0; i < 10; i++) {
+                    var animalDiv = $("<div>");
+                    var p = $("<p>");
+                    p.text("Rating: " + results[i].rating);
+                    var animalImage = $("<img>");
+                    animalImage.attr("src", results[i].images.fixed_height_still.url);
+                    animalImage.attr("still", results[i].images.fixed_height_still.url);
+                    animalImage.attr("animated", results[i].images.fixed_height.url);
+                    animalImage.attr("class", "image");
+                    animalDiv.append(p);
+                    animalDiv.append(animalImage);
+                    $("#gifArea").prepend(animalDiv);
+                }
+                $("img").on("click", function () {
+                    var state = $(this).attr("class");
+                    if (state === "image") {
+                        $(this).attr("src", $(this).attr("animated"));
+                        $(this).attr("class", "gif");
+                    } else {
+                        $(this).attr("src", $(this).attr("still"));
+                        $(this).attr("class", "image")
+                    }
+                });
+            });
+        });
+    }
+
+    makeButtons();
+    $(document).on("click", ".animalButton", makeGifs);
+});
